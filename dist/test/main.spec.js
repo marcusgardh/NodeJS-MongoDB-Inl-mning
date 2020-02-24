f289643490c45c31d35745dce72c4373969105c8
marcusgardh <58325557+marcusgardh@users.noreply.github.com>
2020-02-24 11:09:49 +0100

    Test tas inte med i dist

--- dist/test/main.spec.js
+++
@@ -1,9 +0,0 @@
-const mocha = require("mocha");
-const expect = require("chai").expect;
-const app = require("../main");
-
-describe("Todo app main test", () => {
-    it("Should check if app exists", () => {
-        expect(app).to.exist;
-    });
-});
